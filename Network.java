ca39f7df1adbe5db303dab8d58c41014a8d2f29e
AsUprox <ashuprox@gmail.com>
2021-11-05 16:19:32 +0200

    Классы Телефон и Сеть мобильного оператора
    Создайте класс Phone (Телефон) одним из свойств должен быть его номер. Создайте
    класс Network (сеть мобильного оператора). В классе Телефон должны быть описаны
    следующие методы:
    ●
    Регистрация в сети мобильного оператора
    ●
    Метод реализующий исходящий звонок. Данный метод принимает один параметр
    (описывающий номер мобильного телефона). Логика работы этого метода такова:
    если текущий телефон не прошел регистрацию в сети, то закончить работу метода с
    сообщением об этом. Если текущий телефон прошел регистрацию и в сети также
    зарегистрирован телефон на номер которого совершается вызов, то вызвать метод
    входящий звонок у того телефона. Если телефон на номер которого вы совершаете
    вызов в сети не зарегистрирован, то закончить работу метода с сообщением об
    этом.
    ●
    Метод реализующий входящий звонок. Принимает параметр в виде номера с
    которого произвели вызов на текущий. Вывести сообщение вида вам звонит номер
    такой то.

---
+++ Network.java
@@ -0,0 +1,58 @@
+package phones;
+
+public class Network {
+    private static PhoneNumber[] numbers;
+    private static final String[] simCardsPull = { "0504657655", "0501234567", "0508364574" };
+
+    public Network() {
+        numbers = new PhoneNumber[simCardsPull.length];
+        for (int i = 0; i < simCardsPull.length; i++) {
+            numbers[i] = new PhoneNumber(simCardsPull[i]);
+        }
+    }
+
+    public String getFreeNumber(Phone imei) {
+        for (int i = 0; i < simCardsPull.length; i++) {
+            if (numbers[i].isFree()) {
+                numbers[i].setUsed(imei);
+                return numbers[i].getNom();
+            }
+        }
+        return ("");
+    }
+
+    public static String call (String from, String to) {
+        if (from != null && from.equals(to)) {
+            return ("Bip-bip-bip...");
+        }
+        int i = 0;
+        while (i < simCardsPull.length) {
+            if ((numbers[i].imei != null) && numbers[i].nom.equals(to)) {
+                System.out.println (numbers[i].imei.incomingDial(from));
+                return ("Hello!");
+            }
+            i++;
+        }
+        return ("Unknown number " + to);
+    }
+
+    private static class PhoneNumber {
+        private String nom;
+        private Phone imei;
+
+        public PhoneNumber(String nom) {
+            this.nom = nom;
+            this.imei = null;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setUsed(Phone imei) { this.imei = imei; }
+
+        public boolean isFree() {
+            return (this.imei == null);
+        }
+    }
+}
